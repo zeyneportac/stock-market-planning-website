c31f015faa4692574b6f6bf561878feabde8fc04
Elif Yaren TAT <elifyarentat@gmail.com>
2021-05-17 21:52:14 +0300

    refactor: balance list revised

--- src/app/pages/balance-list/balance-list.component.ts
+++ src/app/pages/balance-list/balance-list.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { BalanceService } from '../../utils';
+import { AuthService, BalanceService } from '../../utils';
 import { TranslateService } from '@ngx-translate/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogWindowComponent } from '../../components';
-import { Balance } from '../../models';
+import { Balance, Roles } from '../../models';
 
 @Component({
   selector: 'app-balance-list',
@@ -13,11 +13,16 @@ import { Balance } from '../../models';
 })
 export class BalanceListComponent implements OnInit {
   constructor(
+    private _authService: AuthService,
     private _balanceService: BalanceService,
     private _snackBar: MatSnackBar,
     private _translateService: TranslateService,
     private _dialog: MatDialog
   ) {}
+  isAdmin: boolean =
+    [Roles.Admin].indexOf(
+      this._authService.currentUserValue?.result?.UserTypeName
+    ) !== -1;
 
   balanceList: Array<Balance>;
   searchText: string;
